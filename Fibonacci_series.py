fa3e2b7d2ece763716c526a3c4f06333de473180
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-06-08 15:56:30 +0530

    Calculate Fibonacci series of n numbers

---
+++ Fibonacci_series.py
@@ -0,0 +1,16 @@
+# Function to compute Fibonacci number
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+# Taking input from user for number of terms
+num = int(input("Enter number of terms: "))
+
+# Printing Fibonacci series
+print("Fibonacci series is:")
+for i in range(num):
+    print(fibonacci(i))  # Calling fibonacci function and printing each term in the series
