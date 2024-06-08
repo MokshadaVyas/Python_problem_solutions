faadaeb254c240762f1c335dcf46ce15873e8fbd
Mokshada Vyas <144912196+MokshadaVyas@users.noreply.github.com>
2024-06-08 13:57:11 +0530

    Added basic addition program
    Created a simple Python program that takes two numbers as input and computes their sum.

---
+++ Addition_of_numbers.py
@@ -0,0 +1,9 @@
+#user input of two numbers
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+# performing addition of entered number
+add=num1+num2
+
+# Display the result of the addition
+print("Addition of two number is:-",add)
